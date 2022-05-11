ca32c9e454ea1b4b1acad3015aff1396f1479b30
tvgc <thales.v.gc@hotmail.com>
2022-05-11 20:26:55 -0300

    implementado onclick

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function display05() {
+    document.querySelector('#passo05').classList.add('display-block');
+}
+
+function display06() {
+    document.querySelector('#passo06').classList.add('display-block');
+}
+
+function display10() {
+    document.querySelector('#passo10').classList.add('display-block');
+}
